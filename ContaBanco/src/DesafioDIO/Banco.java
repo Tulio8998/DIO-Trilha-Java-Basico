425405b29574e6245922d909a275d9f59e2797f3
tulio <tulio89lopes@gmail.com>
2024-05-02 19:13:23 -0300

    Desafio DIO É feito algumas melhorias no programa do desafio pedido.

--- ContaBanco/src/DesafioDIO/Banco.java
+++ ContaBanco/src/DesafioDIO/Banco.java
@@ -1,5 +1,6 @@
 package DesafioDIO;
 
+
 import java.util.ArrayList;
 import java.util.List;
 
--- ContaBanco/src/DesafioDIO/ContaTerminal.java
+++ ContaBanco/src/DesafioDIO/ContaTerminal.java
@@ -1,8 +1,6 @@
 package DesafioDIO;
 
 import java.util.Scanner;
-import java.util.List;
-import java.util.ArrayList;
 
 public class ContaTerminal {
 	
